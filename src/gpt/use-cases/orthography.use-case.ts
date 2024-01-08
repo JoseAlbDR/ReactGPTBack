baafead4b4ee6c4cc899024e063c70ee99f9cc90
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2024-01-08 13:07:52 +0100

    feat: better prompt/instructions with output example

--- src/gpt/use-cases/orthography.use-case.ts
+++ src/gpt/use-cases/orthography.use-case.ts
@@ -10,7 +10,24 @@ export const orthographyCheckUseCase = async (
   const { prompt } = options;
 
   const completion = await openai.chat.completions.create({
-    messages: [{ role: 'assistant', content: prompt }],
+    messages: [
+      {
+        role: 'system',
+        content: `
+            You will be provided with texts in english with possible orthography and grammatical mistakes. You have to answer in JSON format, your task is to correct them and answer back possible solutions, you also have to give a accuracy percentage.
+
+            If there are no errors, you have to answer a congratulation message
+
+            Example of output/answer:
+            {
+              userScore: number,
+              errors: string[], // ["error => solution"],
+              message: string // Use emojis and text to give feedback to user
+            }
+          `,
+      },
+      { role: 'user', content: prompt },
+    ],
     model: 'gpt-3.5-turbo',
   });
 
