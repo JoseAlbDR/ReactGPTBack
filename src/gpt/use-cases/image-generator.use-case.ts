115bb275517f2a0770c1341532e1887c96110c0b
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2024-01-24 16:11:53 +0100

    feat: edit image with original image + mask

--- src/gpt/use-cases/image-generator.use-case.ts
+++ src/gpt/use-cases/image-generator.use-case.ts
@@ -1,5 +1,7 @@
 import OpenAI from 'openai';
-import { downloadImageAsPng } from 'src/helpers';
+import * as fs from 'fs';
+import * as path from 'path';
+import { downloadBase64ImageAsPng, downloadImageAsPng } from 'src/helpers';
 
 interface Options {
   prompt: string;
@@ -11,20 +13,45 @@ export const imageGeneratorUseCase = async (
   openai: OpenAI,
   { prompt, originalImage, maskImage }: Options,
 ) => {
-  const response = await openai.images.generate({
-    model: 'dall-e-3',
+  if (!originalImage || !maskImage) {
+    const response = await openai.images.generate({
+      model: 'dall-e-3',
+      prompt,
+      n: 1,
+      quality: 'standard',
+      style: 'vivid',
+      response_format: 'url',
+      size: '1024x1024',
+    });
+
+    const url = await downloadImageAsPng(response.data[0].url);
+
+    return {
+      url, // TODO: http://localhost:3000/...
+      openAIUrl: response.data[0].url,
+      revised_prompt: response.data[0].revised_prompt,
+    };
+  }
+
+  const pngImagePath = await downloadImageAsPng(originalImage);
+  const maskPath = await downloadBase64ImageAsPng(maskImage);
+
+  const response = await openai.images.edit({
+    model: 'dall-e-2',
     prompt,
+    mask: fs.createReadStream(maskPath),
+    image: fs.createReadStream(pngImagePath),
     n: 1,
-    quality: 'standard',
-    style: 'vivid',
-    response_format: 'url',
     size: '1024x1024',
+    response_format: 'url',
   });
 
-  const url = await downloadImageAsPng(response.data[0].url);
+  const localImagePath = await downloadImageAsPng(response.data[0].url);
+  const fileName = path.basename(localImagePath);
+  const publicUrl = `localhost:3000/${fileName}`;
 
   return {
-    url,
+    url: publicUrl, // TODO: http://localhost:3000/...
     openAIUrl: response.data[0].url,
     revised_prompt: response.data[0].revised_prompt,
   };
--- src/helpers/download-image-as-png.ts
+++ src/helpers/download-image-as-png.ts
@@ -18,11 +18,25 @@ export const downloadImageAsPng = async (url: string) => {
 
   const buffer = Buffer.from(await response.arrayBuffer());
 
-  // fs.writeFileSync(`${folderPath}/${imageNamePng}`, buffer);
-
   const completePath = path.join(folderPath, imageNamePng);
 
   await sharp(buffer).png().ensureAlpha().toFile(completePath);
 
   return completePath;
+};
+
+export const downloadBase64ImageAsPng = async (base64Image: string) => {
+  base64Image = base64Image.split(';base64,').pop();
+  const imageBuffer = Buffer.from(base64Image, 'base64');
+
+  const folderPath = path.resolve('./', './generated/images/');
+  fs.mkdirSync(folderPath, { recursive: true });
+
+  const imageNamePng = `${new Date().getTime()}-64.png`;
+
+  const completePath = path.join(folderPath, imageNamePng);
+
+  await sharp(imageBuffer).png().ensureAlpha().toFile(completePath);
+
+  return path.join(folderPath, imageNamePng);
 };
